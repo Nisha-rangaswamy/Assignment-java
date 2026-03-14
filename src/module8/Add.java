36d4f3416af90aa82ae00c673fdd4c1fcbce508a
Nisha-rangaswamy <nishagowda1973@gmail.com>
2026-03-14 11:49:32 +0530

    This is trial

---
+++ src/module8/Add.java
@@ -0,0 +1,12 @@
+package module8;
+
+public class Add {
+    public static void main(String[] args) {
+        int a=10;
+        int c=20;
+        int b=c+a;
+        System.out.println(c);
+    }
+
+
+}
